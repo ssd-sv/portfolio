79548e56e7ff650dde6d3d6572f4a14ea5b4850d
anudasa <shsrdas@gmail.com>
2021-01-24 18:03:20 +0300

    Styling Contact-page

---
+++ js/textarea.js
@@ -0,0 +1,7 @@
+let textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keyup', function(){
+  if(this.scrollTop > 0){
+    this.style.height = this.scrollHeight + "px";
+  }
+});
