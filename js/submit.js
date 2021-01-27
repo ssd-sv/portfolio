18f67f8bdf994b3f66ed4b445c87182575c1a35a
anudasa <shsrdas@gmail.com>
2021-01-27 17:47:02 +0300

    Mobile menu upgrading and litle changes. Delite excess files.

--- js/submit.js
+++ js/submit.js
@@ -1,10 +1,5 @@
 let infoMessage = document.querySelector('.contact-form__text-after-submit');
-let form = document.querySelector('.contact-form');
-let name = document.querySelector('.contact-form__input');
-let email = document.querySelector('.contact-form__input-email');
-let message = document.querySelector('.contact-form__textarea');
 
 form.onsubmit = function() {
-  // evt.preventDefault();
   infoMessage.textContent = 'Thank you! I will contact you as soon as possible';
 };
